9e8f54fe7d8f71fae73bc32fdd9c1a3d40059671
zeevnoiman <zevnoiman@gmail.com>
2023-08-19 22:45:36 +0300

    remove some irrelevant code from index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import Fastify from "fastify";
-import { DbService } from "./services/db.service";
-import { ItemsService } from "./services/items.service";
-
 
 const fastify = Fastify({
     logger: true,
@@ -17,24 +14,4 @@ fastify.listen({ port }, function (err, address) {
   }
  
   fastify.log.info(`Fastify is listening on port: ${address}`);
-});
-
-const mongoUri = "mongodb://root:example@localhost:27017/Items";
-const db = new DbService(mongoUri);
-const itemsService = new ItemsService(db);
-
-const description = "My Item";
-itemsService.createItem(description);
-itemsService.updateItem(description, {
-    address: "added 1",
-    city: "New York",
-    state: "NY",
-    zip: "30001"
-});
-
-itemsService.updateItem(description, {
-    address: "added 2",
-    city: "New York",
-    state: "NY",
-    zip: "40001"
 });
